1e7f50a942c63bc1ac5d5ac7029eb0579be7d69d
Ane Grizotti <algrizotti@gmail.com>
2022-02-24 21:05:57 -0300

    Ajustes

--- Flavius_Josephus.ConsoleApp/Program.cs
+++ Flavius_Josephus.ConsoleApp/Program.cs
@@ -6,32 +6,52 @@ namespace Flavius_Josephus.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.Write("Digite a quantidade de homens: ");
-            int qntHomens = int.Parse(Console.ReadLine());
 
-            Console.Write("Digite o tamanho do salto: ");
-            int tamSalto = int.Parse(Console.ReadLine());
+            Console.Write("Digite o número de homens: ");
+            int qntHomens = int.Parse( Console.ReadLine());
 
-            int[] qntHomensArray = new int[qntHomens];
-            int index = 1;
+            int[] homens = new int[qntHomens+1];
 
-            for (int i = 0; i < qntHomensArray.Length; i++)
+            Console.Write("Digite o tamanho do salto entre os homens: ");
+            int tamanhoSalto = int.Parse(Console.ReadLine());
+
+            for (int i=1; i<=qntHomens; i++)
             {
-                qntHomensArray[i] = index;
-                index++;
-            }
-            
-            int ultimoHomem = 0;
-
-            
-            for (int i = 0;i < qntHomensArray.Length ;i=i+tamSalto)
-            {
-                qntHomensArray[i] = -1;
+                homens[i] = i;
             }
 
+            int j = 1, x = 0, y = 0;
 
+            while (x != qntHomens-1)
+            {
+                if (homens[j] != 0)
+                {
+                    y++;
 
-            Console.WriteLine(ultimoHomem);
+                    if (y == tamanhoSalto)
+                    {
+                        homens[j] = 0;
+                        x++;
+                        y = 0;
+                    }
+                }
+
+                j++;
+
+                if (j > qntHomens)
+                {
+                    j = 1;
+                }
+            }
+
+            for(int i=1; i <= qntHomens; i++)
+            {
+                if (homens[i] != 0)
+                {
+                    Console.WriteLine(homens[i]+ " sobreviveu!");
+                    break;
+                }
+            }
         }
     }
 }
